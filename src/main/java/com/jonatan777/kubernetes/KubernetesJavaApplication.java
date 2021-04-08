6330611cad4a724acfff3ff158e11f3699ac11ba
jonatan777 <jonatan777@gmail.com>
2021-04-08 19:18:28 -0300

    primeiro commit

---
+++ src/main/java/com/jonatan777/kubernetes/KubernetesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jonatan777.kubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonatan777/kubernetes/KubernetesJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonatan777.kubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
